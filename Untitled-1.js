74e6a33cf3bd1fbac2840b95d7e27bb5a36aa562
user134383097489237489 <94034091+user134383097489237489@users.noreply.github.com>
2021-11-10 14:16:48 +0800

    Add files via upload

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,20 +1,10 @@
 
-
-
-
-    const response = await fetch('https://api.blooket.com/api/users/add-rewards', {
-        method: "PUT",
-        headers: {
-            "referer": "https://www.blooket.com/",
-            "content-type": "application/json",
-            "authorization": localStorage.token
-        },
         body: JSON.stringify({
             addedTokens: 500,
             addedXp: 300,
             name: await getName(myToken)
         })
-    });
+    
 
     if (response.status == 200) {
         alert(`500} tokens and 300 XP added to your account!`);
