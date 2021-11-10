293148afab1db32cee1c4bba37e778ec452e34a2
user134383097489237489 <94034091+user134383097489237489@users.noreply.github.com>
2021-11-10 14:10:41 +0800

    Add files via upload

--- Untitled-1.js
+++ Untitled-1.js
@@ -5,13 +5,7 @@ async function getName(authToken) {
     return data.name
 };
 
-async function addCurrencies() {
-    const add_tokens = Number(prompt('How many tokens do you want to add to your account? (500 daily)'));
-    const myToken = localStorage.token.split('JWT ')[1];
 
-    if (add_tokens > 500) {
-        alert('You can add up to 500 tokens daily.')
-    }
 
     const response = await fetch('https://api.blooket.com/api/users/add-rewards', {
         method: "PUT",
@@ -21,18 +15,17 @@ async function addCurrencies() {
             "authorization": localStorage.token
         },
         body: JSON.stringify({
-            addedTokens: add_tokens,
+            addedTokens: 500,
             addedXp: 300,
             name: await getName(myToken)
         })
     });
 
     if (response.status == 200) {
-        alert(`${add_tokens} tokens and 300 XP added to your account!`);
-    } else {
-        alert('An error occured.');
+        alert(`500} tokens and 300 XP added to your account!`);
+    
     };
 
-};
+
 
 addCurrencies();
